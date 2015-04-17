5fd3e4d32fe1635981c6375524730674f32392f6
Jan Godziatkowski <ph.godzio@gmail.com>
2015-04-17 11:23:29 +0200

    Test 1 Red

--- src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
+++ src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
@@ -1,16 +1,26 @@
 package edu.iis.mto.serverloadbalancer;
 
-
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
 import org.junit.Test;
 
 public class ServerLoadBalancerTest {
-	@Test
-	public void itCompiles() {
-		assertThat(true, equalTo(true));
-	}
 
+    @Test
+    public void itCompiles() {
+        assertThat( true, equalTo( true ) );
+    }
+
+    @Test
+    public void balancingServer_oneServer_zeroVM_serverEmpty() {
+        Server server = a( server().withCapacity( 1 ) );
+
+        balance( aListOfServers( server ), aListOfVM() );
+    }
+
+    private Object server() {
+        throw new UnsupportedOperationException( "Not supported yet." ); //To change body of generated methods, choose Tools | Templates.
+    }
 
 }
