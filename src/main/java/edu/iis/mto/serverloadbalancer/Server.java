b5cf08335327477e36842edd8c19e6147114c6e5
Jan Godziatkowski <ph.godzio@gmail.com>
2015-05-15 09:49:30 +0200

    Test 3 Green

--- src/main/java/edu/iis/mto/serverloadbalancer/Server.java
+++ src/main/java/edu/iis/mto/serverloadbalancer/Server.java
@@ -1,15 +1,16 @@
 package edu.iis.mto.serverloadbalancer;
 
-
 public class Server {
 
-	public double currentLoadPecentage;
+    public double currentLoadPecentage;
+    public final int capacity;
 
-	public Server(int capacity) {
-	}
+    public Server( int capacity ) {
+        this.capacity = capacity;
+    }
 
-	public boolean contains(Vm vm) {
-		return true;
-	}
+    public boolean contains( Vm vm ) {
+        return true;
+    }
 
 }
